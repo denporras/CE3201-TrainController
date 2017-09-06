e4e07374ef8352617661fb606cdace6bb4dc5c0a
Dennis <denporras07@gmail.com>
2017-09-06 15:05:31 -0600

    First Commit

---
+++ Parameters.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    14:51:08 09/06/2017 
+// Design Name: 
+// Module Name:    Parameters 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module Parameters(
+    input A,
+    input B,
+    input C,
+    input D,
+    output T
+    );
+
+
+endmodule
---
+++ README.md
@@ -0,0 +1,2 @@
+# CE3201-TrainController
+En este  laboratorio se  requiere  que  el  grupo  de  trabajo  diseñe, implemente  en un  HDL y demuestre su funcionamiento con un kit FPGA, un controlador para un sistema de dispositivos de señalización  y  prevención  (semáforo  y  agujas)  para  un  cruce  de  vía  vehicular-ferroviaria. Este controlador  estará  basado  en  una  máquina  de  estados  finitos  (FSM), además  incluirá  la  lógica combinacional y secuencial necesaria para apoyar a la máquina de estados.

3add8d065920607761a8645fc5d63b7a206f1a7f
Dennis <denporras07@gmail.com>
2017-09-06 16:25:11 -0600

    Standard Comments

--- Parameters.v
+++ Parameters.v
@@ -1,19 +1,19 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: Dennis Porras, David Gómez and Kelvin Alfaro
 // 
 // Create Date:    14:51:08 09/06/2017 
-// Design Name: 
+// Design Name: 	Modulo de Parametros
 // Module Name:    Parameters 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
+// Project Name: Train Controller
+// Target Devices: FPGA
+// Tool versions: 1.0
+// Description: Modulo para la verificacion de parámetros
 //
 // Dependencies: 
 //
-// Revision: 
+// Revision: 1
 // Revision 0.01 - File Created
 // Additional Comments: 
 //

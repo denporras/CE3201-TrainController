0b088bb70dacbf2980bcc4972a89ddeed683f376
Daedgomez <daedgomez@gmail.com>
2017-09-13 18:38:37 -0600

    [Add] Top level union

--- TopLevelModule.v
+++ TopLevelModule.v
@@ -18,15 +18,16 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module TopLevelModule(  bcd , an, seg7 );
+module TopLevelModule(S1,S2,S3,S4,S5,S6);
 
-output [3:0] an;
-reg [3:0] an;
-output [6:0] seg7 ;
-reg [6:0] seg7 ;
-
-input [3:0] bcd ;
-wire [3:0] bcd ;
+input  S1,S2,S3,S4,S5,S6;
+wire next_state, enable, y;
+Counter counter(.next_state(next_state),.ld(),.en(),.clk(),.present_state());
+Counter_comb counter_comb(.next_state(next_state),.ld(),.en(),.a(),.b(),.c(),.d(),.y());
+//Parameters parameters(.present_state(),.t(),.clk());
+Predictor predictor(.time_in(),.time_out());
+Selector selector(t1,t0,tout,clk);
+Syncronizer(.S1(S1),.S2(S2),.S3(S3),.S4(S4),.S5(S5),.S6(S6),.Selector(next_state),.Enable(enable),.Y(y));
 
 
 always @ (bcd) begin
